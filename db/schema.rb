c2ed27e6eb709979c8bdc32b31207642f4ed803b
Rico Mitchell <ricomitch@icloud.com>
2020-04-15 01:40:52 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 6) do
+ActiveRecord::Schema.define(version: 8) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 6) do
 
   create_table "catchphrase", force: :cascade do |t|
     t.string "catchphrase"
+    t.string "character"
   end
 
   create_table "characters", force: :cascade do |t|
@@ -35,6 +36,9 @@ ActiveRecord::Schema.define(version: 6) do
   create_table "shows", force: :cascade do |t|
     t.string "name"
     t.integer "network_id"
+    t.string "day"
+    t.string "genre"
+    t.string "season"
   end
 
 end
