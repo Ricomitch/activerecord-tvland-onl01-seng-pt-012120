2619f1d3a35629d99c5491cb92ca1d5eadd46477
Rico Mitchell <ricomitch@icloud.com>
2020-04-15 01:33:51 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
-end
+
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+      list =[]
+      self.actors.map{|a| list << "#{a.full_name}"}
+      list
+    end
+  end
